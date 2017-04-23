347335614a3f3fcc3d2929adffd8d5d8113891c7
Sergey Morozov <se.mrzv@gmail.com>
2017-04-23 11:10:53 +0300

    apdated urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.test, name='test'),  
+    url(r'^$', views.test, name='test'),
     url(r'^login/', views.login, name='login'),
     url(r'^signup/', views.test, name='test'),
-    url(r'^web/', views.test, name='test'),
+    url(r'^ask/', views.test, name='test'),
     url(r'^new/', views.test, name='test'),
     url(r'^popular/', views.test, name='test'),
     url(r'^question/(\d+)/', views.test, name='test'),
